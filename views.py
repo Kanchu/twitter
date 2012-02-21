787b052823b775ac841f4bfd32ef3c69fe044b99
Kanchan Motlag <kanchan.motlag09@gmail.com>
2012-02-21 16:42:28 +0530

    removed unused import

--- views.py
+++ views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import logout
-from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response, redirect
 from django.template.context import RequestContext
